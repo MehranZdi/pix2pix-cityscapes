808462cf7a7caf5d254dbbf6c4037e599fc571d2
MehranZdi <zeidimehran@gmail.com>
2025-05-22 17:26:57 +0200

    Add train module.

---
+++ train.py
@@ -0,0 +1,136 @@
+import torch
+import torch.nn as nn
+import os
+from datasets import load_dataset
+from torch.utils.data import DataLoader
+from torchvision.transforms import transforms
+from dataset import EdgesToShoesDataset
+from sklearn.model_selection import train_test_split
+from model import UNetGenerator, Discriminator
+import torch.optim as optim
+from tqdm import tqdm
+
+
+
+def train_one_epoch(
+        generator,
+        discriminator,
+        dataloader,
+        optimizer_G,
+        optimizer_D,
+        bce_loss,
+        l1_loss,
+        device,
+        lambda_l1=100
+):
+    
+    generator.train()
+    discriminator.train()
+
+    pbar = tqdm(dataloader, desc="Training", leave=False)
+    total_g_loss, total_d_loss = 0.0, 0.0
+
+    for batch in pbar:
+        input_img = batch["input"].to(device)
+        target_img = batch["target"].to(device)
+
+        # Train Discriminator
+
+        fake_img = generator(input_img)
+        D_real = discriminator(torch.cat([input_img, target_img], dim=1))
+        real_labels = torch.ones_like(D_real).to(device)
+        loss_real = bce_loss(D_real, real_labels)
+
+        # Fake pair (detach to avoid G updates)
+        D_fake = discriminator(torch.cat([input_img, fake_img.detach()], dim=1))
+        fake_labels = torch.zeros_like(D_fake).to(device)
+        loss_fake = bce_loss(D_fake, fake_labels)
+
+        d_loss = (loss_real + loss_fake) * 0.5
+        optimizer_D.zero_grad()
+        d_loss.backward()
+        optimizer_D.step()
+
+        # Train Generator
+
+        D_fake_for_G = discriminator(torch.cat([input_img, fake_img], dim=1))
+        valid_labels = torch.ones_like(D_fake_for_G).to(device)
+
+        g_adv_loss = bce_loss(D_fake_for_G, valid_labels)
+        g_l1_loss = l1_loss(fake_img, target_img) * lambda_l1
+        g_loss = g_adv_loss + g_l1_loss
+
+        optimizer_G.zero_grad()
+        g_loss.backward()
+        optimizer_G.step()
+
+        total_g_loss += g_loss.item()
+        total_d_loss += d_loss.item()
+
+        pbar.set_postfix({
+            "G_loss": f"{g_loss.item():.4f}",
+            "D_loss": f"{d_loss.item():.4f}"
+        })
+
+        avg_g_loss = total_g_loss / len(dataloader)
+        avg_d_loss = total_d_loss / len(dataloader)
+        return avg_g_loss, avg_d_loss
+    
+
+def train_model(
+        generator,
+        discriminator,
+        dataloader,
+        optimizer_G,
+        optimizer_D,
+        device,
+        num_epochs=20,
+        lambda_l1=100,
+        checkpoint_dir="checkpoints"
+):
+    bce_loss = nn.BCEWithLogitsLoss()
+    l1_loss = nn.L1Loss()
+    os.makedirs(checkpoint_dir, exist_ok=True)
+
+    for epoch in range(num_epochs):
+        print(f"\nEpoch {epoch+1} / {num_epochs}")
+        avg_g_loss, avg_d_loss = train_one_epoch(
+            generator, 
+            discriminator,
+            dataloader,
+            optimizer_G,
+            optimizer_D,
+            bce_loss,
+            l1_loss,
+            device,
+            lambda_l1
+        )
+    
+        print(f"Epoch {epoch+1}: G_loss = {avg_g_loss:.4f}, D_loss = {avg_d_loss:.4f}")
+
+        torch.save(generator.state_dict(), f"checkpoints/generator_epoch_{epoch+1}.pt")
+        torch.save(discriminator.state_dict(), f"checkpoints/discriminator_epoch_{epoch+1}.pt")
+
+
+if __name__ == '__main__':
+    
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f"The model is training on {device}...")
+    dataset = load_dataset("huggan/edges2shoes", split="train")
+    train_indices, val_indices = train_test_split(range(len(dataset)), test_size=0.2, random_state=24, shuffle=True)
+    train_data = dataset.select(train_indices)
+    val_data = dataset.select(val_indices)
+
+    train_dataset = EdgesToShoesDataset(train_data)
+    val_dataset = EdgesToShoesDataset(val_data)
+
+    train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True, num_workers=4)
+    val_loader = DataLoader(val_dataset, batch_size=16, num_workers=4)
+    
+    generator = UNetGenerator().to(device)
+    discriminator = Discriminator().to(device)
+
+    optimizer_G = torch.optim.Adam(generator.parameters(), lr=2e-4, betas=(0.5, 0.999))
+    optimizer_D = torch.optim.Adam(discriminator.parameters(), lr=2e-4, betas=(0.5, 0.999))
+
+    train_model(generator, discriminator, train_loader, optimizer_G, optimizer_D, device, num_epochs=1)
