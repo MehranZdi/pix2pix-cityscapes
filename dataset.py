310cdcc0e15841ed968084fe0ebc32aca5dfaf8f
MehranZdi <zeidimehran@gmail.com>
2025-05-27 15:17:53 +0200

    Modify transforms to prevent underfitting.

--- dataset.py
+++ dataset.py
@@ -1,58 +1,66 @@
-from torchvision.transforms import transforms
-from torch.utils.data import DataLoader, Dataset
+from torchvision import transforms
+from torch.utils.data import Dataset, DataLoader
 from PIL import Image
 import os
+import random
 
 class CityscapesDataset(Dataset):
-    def __init__(self, path, transform=None, is_train=True):
-        self.img_dir = os.path.join(path + '/img')
-        self.label_dir = os.path.join(path + '/label')
+    def __init__(self, path, is_train=True):
+        self.img_dir = os.path.join(path, 'img')
+        self.label_dir = os.path.join(path, 'label')
 
         self.img_files = sorted(os.listdir(self.img_dir))
         self.label_files = sorted(os.listdir(self.label_dir))
 
-        if is_train:
-            self.transform = transforms.Compose([
-                # Randomly choose between jitter (resize + crop) and simple resize
-                transforms.RandomChoice([
-                    transforms.Compose([
-                        transforms.Resize(286),  # Random jitter
-                        transforms.RandomCrop(256),
-                    ]),
-                    transforms.Resize((256, 256)),  # No jitter
-                ], p=[0.8, 0.2]),  # 80% chance of jitter, 20% chance of no jitter
-                transforms.RandomHorizontalFlip(p=0.5),  # Mirroring
-                transforms.ToTensor(),
-                transforms.Normalize((0.5,), (0.5,))
-            ])
-        else:
-            self.transform = transform or transforms.Compose([
-            transforms.Resize((256, 256)),
-            transforms.ToTensor(),
-            transforms.Normalize((0.5,), (0.5,))
-            ])
+        assert len(self.img_files) == len(self.label_files), "Image and label count mismatch!"
+
+        self.is_train = is_train
+        self.resize = transforms.Resize((286, 286))
+        self.crop = transforms.RandomCrop((256, 256))
+        self.flip = transforms.RandomHorizontalFlip(p=1.0)
+        self.to_tensor = transforms.ToTensor()
+        self.normalize = transforms.Normalize(mean=[0.5]*3, std=[0.5]*3)
 
     def __len__(self):
         return len(self.img_files)
-    
 
     def __getitem__(self, idx):
-        image_path = os.path.join(self.img_dir, self.img_files[idx])
+        img_path = os.path.join(self.img_dir, self.img_files[idx])
         label_path = os.path.join(self.label_dir, self.label_files[idx])
 
-        image = Image.open(image_path).convert("RGB")
+        img = Image.open(img_path).convert("RGB")
         label = Image.open(label_path).convert("RGB")
-        image = self.transform(image)
-        label = self.transform(label)
 
-        return {'image': image, 'label': label}
+        if self.is_train:
+            # Apply same random operations to both input & label
+            img, label = self.resize(img), self.resize(label)
+
+            i, j, h, w = transforms.RandomCrop.get_params(img, output_size=(256, 256))
+            img = transforms.functional.crop(img, i, j, h, w)
+            label = transforms.functional.crop(label, i, j, h, w)
+
+            if random.random() > 0.5:
+                img = transforms.functional.hflip(img)
+                label = transforms.functional.hflip(label)
+
+        else:
+            img = transforms.Resize((256, 256))(img)
+            label = transforms.Resize((256, 256))(label)
+
+        img = self.to_tensor(img)
+        label = self.to_tensor(label)
+
+        img = self.normalize(img)
+        label = self.normalize(label)
+
+        return {"input": label, "target": img}  # ← Pix2Pix: label → photo
 
 
 if __name__ == "__main__":
-    train_path = "/home/mehran/Documents/Pix2Pix/train"
-    val_path = "/home/mehran/Documents/Pix2Pix/val"
-    
+    train_path = "/home/mehran/Datasets/Pix2Pix/Cityscapes/train"
+    val_path = "/home/mehran/Datasets/Pix2Pix/Cityscapes/train"
+
     train_dataset = CityscapesDataset(train_path)
     val_dataset = CityscapesDataset(val_path)
-    train_loader = DataLoader(train_dataset, batch_size=1, shuffle=True, num_workers=2)
+    train_loader = DataLoader(train_dataset, batch_size=1, shuffle=True, num_workers=3)
     val_loader = DataLoader(val_dataset, batch_size=16, num_workers=2)
