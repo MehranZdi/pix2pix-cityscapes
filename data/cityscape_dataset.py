ce6a5d7d2dad333c5d3c2b4316266a9b2be7585f
mehranzdi <zeidimehran@gmail.com>
2025-07-24 11:02:04 +0200

    Fix the input size.

--- data/cityscape_dataset.py
+++ data/cityscape_dataset.py
@@ -1,11 +1,12 @@
 import os
+import torch
 from PIL import Image
 from torch.utils.data import Dataset, DataLoader
-import torchvision.transforms as transforms
+import torchvision.transforms.v2 as transforms
 
 
 class CityscapesDataset(Dataset):
-    def __init__(self, path, is_train=True, target_size=(512, 256)):
+    def __init__(self, path, is_train=True, target_size=(256, 512)):
         self.img_dir = os.path.join(path, 'images')
         self.label_dir = os.path.join(path, 'masks')
 
@@ -31,13 +32,15 @@ class CityscapesDataset(Dataset):
             self.transform = transforms.Compose([
                 transforms.Resize(target_size),
                 transforms.RandomHorizontalFlip(p=0.5),
-                transforms.ToTensor(),
+                transforms.ToImage(),
+                transforms.ToDtype(torch.float32, scale=True),
                 transforms.Lambda(lambda x: x * 2.0 - 1.0),
             ])
         else:
             self.transform = transforms.Compose([
                 transforms.Resize(target_size),
-                transforms.ToTensor(),
+                transforms.ToImage(),
+                transforms.ToDtype(torch.float32, scale=True),
                 transforms.Lambda(lambda x: x * 2.0 - 1.0),
             ])
 
@@ -61,7 +64,7 @@ if __name__ == "__main__":
     train_path = "/home/mehran/Datasets/Cityscapes/train"
     val_path = "/home/mehran/Datasets/Cityscapes/val"
 
-    train_dataset = CityscapesDataset(train_path, is_train=True, target_size=(512, 256))
-    val_dataset = CityscapesDataset(val_path, is_train=False, target_size=(512, 256))
+    train_dataset = CityscapesDataset(train_path, is_train=True, target_size=(256, 512))
+    val_dataset = CityscapesDataset(val_path, is_train=False, target_size=(256, 512))
     train_loader = DataLoader(train_dataset, batch_size=2, shuffle=True, num_workers=4)
     val_loader = DataLoader(val_dataset, batch_size=2, num_workers=4)
